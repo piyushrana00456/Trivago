5acb7e8e7e2c834eff3ff3f91a0a38407592b44a
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-08-09 16:58:20 +0530

    fourth

--- unit-3/evaluation/topNew.js
+++ unit-3/evaluation/topNew.js
@@ -1,4 +1,5 @@
 let wrap1=document.getElementById('data')
+let wrap2=document.getElementById('Wrap')
 
 
 async function topNews(){
@@ -20,23 +21,9 @@ async function topNews(){
          p.innerText=title
 
 
-         p.addEventListener('click',()=>{
-             
-       let wrap2=document.getElementById('Wrap')
+         p.addEventListener('click', function gotoDetail(){ 
 
          window.location.href='homeDetail.html'
-         let detail_div=document.createElement('div')
-         detail_div.setAttribute('class','detail_div')
-
-         let d_p1=document.createElement('p')
-         d_p1.setAttribute('class','detail_p')
-  
-         d_p1.innerText=title
-         detail_div.append(d_p1)
-
-        wrap2.append(detail_div)
-
-
          })
          img.src=urlToImage
 
@@ -47,3 +34,27 @@ async function topNews(){
 }
 topNews()
 
+function gotoDetail(el){
+    
+el.forEach(({title})=>{
+    let detail_div=document.createElement('div')
+    detail_div.setAttribute('class','detail_div')
+
+    let d_p1=document.createElement('p')
+    d_p1.setAttribute('class','detail_p')
+
+    d_p1.innerText=title
+    detail_div.append(d_p1)
+
+   wrap2.append(detail_div)
+
+})
+
+
+  
+}
+
+ function main(){
+    let set= topNews()
+    gotoDetail(set) 
+ }
