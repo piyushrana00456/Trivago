1be086370c60ad15ccf7f08a6075fa75c5248656
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-08-09 17:15:12 +0530

    fifth

--- unit-3/evaluation/topNew.js
+++ unit-3/evaluation/topNew.js
@@ -10,7 +10,7 @@ async function topNews(){
 
     let{articles}=data
     
-    articles.forEach(({title,urlToImage})=> {
+    articles.forEach(({title,urlToImage,description,content})=> {
         let div=document.createElement('div')
         div.setAttribute('class','contain')
         let p=document.createElement('p')
@@ -21,9 +21,34 @@ async function topNews(){
          p.innerText=title
 
 
-         p.addEventListener('click', function gotoDetail(){ 
+         p.addEventListener('click', ()=>{ 
+          wrap1.innerHTML=null
+       
+            let detail_div=document.createElement('div')
+            detail_div.setAttribute('class','detail_div')
+        
+            let d_p1=document.createElement('p')
+            d_p1.setAttribute('class','detail_p')
+            
+            let d_p2=document.createElement('p')
+            d_p2.setAttribute('class','d_p2')
 
-         window.location.href='homeDetail.html'
+           let d_p3=document.createElement('p')
+           d_p3.setAttribute('class','d_p3')
+
+           let d_img=document.createElement('img')
+           d_img.setAttribute('class','image') 
+
+
+
+            d_img.src=urlToImage; 
+            d_p3.innerText=content
+            d_p2.innerText=description
+            d_p1.innerText=title
+            detail_div.append(d_img,d_p1,d_p2,d_p3)
+        
+           wrap1.append(detail_div)
+        
          })
          img.src=urlToImage
 
@@ -36,19 +61,7 @@ topNews()
 
 function gotoDetail(el){
     
-el.forEach(({title})=>{
-    let detail_div=document.createElement('div')
-    detail_div.setAttribute('class','detail_div')
 
-    let d_p1=document.createElement('p')
-    d_p1.setAttribute('class','detail_p')
-
-    d_p1.innerText=title
-    detail_div.append(d_p1)
-
-   wrap2.append(detail_div)
-
-})
 
 
   
